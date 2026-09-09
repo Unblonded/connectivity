c3c3b54ece75d5148a94d4920e7f336a0fb77731
Unblonded <edmundedj@gmail.com>
2026-09-09 11:56:56 -0700

    Initial Commit

---
+++ connectivity/connectivityApp.swift
@@ -0,0 +1,32 @@
+//
+//  connectivityApp.swift
+//  connectivity
+//
+//  Created by Edmund Edjhuryan on 9/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct connectivityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ connectivityTests/connectivityTests.swift
@@ -0,0 +1,19 @@
+//
+//  connectivityTests.swift
+//  connectivityTests
+//
+//  Created by Edmund Edjhuryan on 9/9/26.
+//
+
+import Testing
+@testable import connectivity
+
+struct connectivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
